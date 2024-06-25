9438712046f6bf9572a8455f989bdd1b1aa70386
Sashrik Gupta <159465707+SashrikGupta@users.noreply.github.com>
2024-06-26 02:11:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -94,5 +94,7 @@ def delete_pdf():
         return "PDF file not found", 404
 
 if __name__ == '__main__':
+    if not os.path.exists("uploads"):
+        os.makedirs("uploads")
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
